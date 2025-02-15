c6e64bba878979e754b8fcde2765c6fd57014b82
inam <inamzeynalli01@gmail.com>
2025-02-15 22:03:07 +0400

    12345

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,6 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(first,second) {
+    calculateAverage =(first+second)/2
+    return calculateAverage;
+}
